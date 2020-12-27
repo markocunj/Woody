8491e877adf7f505c372b6b2dd6ab3ecfb52b616
Pejdo <58657684+Pejdo@users.noreply.github.com>
2020-12-27 19:24:22 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home </router-link>
-      <router-link to="/prijava">Prijava </router-link>
-      <router-link to="/registracija">Registracija</router-link>
-    </div>
-    <router-view />
+   
+  <div id="app"> 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">nesto sto treba </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Prijava</router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link to="/singup" class="nav-link"
+              >Registracija</router-link
+            >
+          </li>
+        </ul>
+        <button type="button">stisni</button>
+      </div>
+    </nav>
+      <router-view />
   </div>
 </template>
 
-<style lang="scss" scoped>
-@import "./assets/style/woody.css";
-div {
-  color: inherit;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #c2b9b9;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss">
+  @import "./assets/style/woody.css";
+  @import "./assets/style/media.css";
 </style>
