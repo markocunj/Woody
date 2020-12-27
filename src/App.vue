81d55397b3cdde5083da6c37606b766c2a95a4ae
Pejdo <58657684+Pejdo@users.noreply.github.com>
2020-12-27 21:49:48 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
           <li class="nav-item active">
             <router-link to="/" class="nav-link">Home</router-link>
           </li>
-          <li class="nav-item">
+          <li class="nav-item ">
             <router-link to="/login" class="nav-link">Prijava</router-link>
           </li>
           <li class="nav-item active">
---
+++ src/views/Singup.vue
@@ -0,0 +1,112 @@
+<template> 
+<div class="col-md-12">
+  <div class="row">
+    <div class="col-md-6 register-left">
+      <img class="biglogo" alt="Vue logo" src="../assets/logo.png" />
+    </div> 
+     <div class="col-md-6 well">
+    <div class="container jumbotrontop ">
+      <h1 class="headline">Registracija</h1>
+        <div class="row">
+          <form>
+            <div class="col-sm-12">
+              <div class="row">
+                <div class="col-sm-6">
+                  <label id="email">Email Address</label>
+                  <input
+                    type="text"
+                    placeholder="Enter Email Address Here.."
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <label id="ime">Ime</label>
+                  <input
+                    type="text"
+                    placeholder="pr.Ivan"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <label id="prezime">Prezime</label>
+                  <input
+                    type="text"
+                    placeholder="pr.Horvat"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-6">
+                  <label id="pass">Password</label>
+                  <input
+                    type="text"
+                    placeholder="Password"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-6">
+                  <label id="repeat">Repeat Password</label>
+                  <input
+                    type="text"
+                    placeholder="Password"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-3">
+                  <label id="year">Godina</label>
+                  <input
+                    type="text"
+                    id="godina"
+                    placeholder="pr.1994"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-3">
+                  <label id="day">Dan</label>
+                  <input
+                    type="text"
+                    id="mh"
+                    placeholder="pr.12"
+                    class="form-control"
+                  />
+                </div>
+                <div class="col-sm-3">
+                  <label id="month">Mjesec</label>
+                  <input
+                    type="text"
+                    id="dan"
+                    placeholder="pr.Siječanj"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Inventore dicta soluta quis iure asperiores consequatur, eum
+                    aliquid! Neque voluptatem,
+                  </p>
+                  <input type="checkbox" />
+                  <span class="checkmark"></span>
+                  <label>Šaljite mi mailove</label>
+                </div>
+              </div>
+              <div class="regi">
+                <button type="button" class="btn btn-lg btn-info">
+                  Registracija
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
