95d9b0cfecf055e928c41f82ab0853f56ed926e2
Pejdo <58657684+Pejdo@users.noreply.github.com>
2020-12-27 19:20:44 +0100

    Update Registracija.vue

--- src/views/Registracija.vue
+++ src/views/Registracija.vue
@@ -1,124 +1,50 @@
 <template>
-  <div class="row">
-    <div class="col-md-3 register-left">
-      <img alt="Vue logo" src="../assets/logo.png" />
-    </div>
-    <div class="container">
-      <h1 class="headline">Registracija</h1>
-      <div class="col-lg-12 well">
-        <div class="row">
-          <form>
-            <div class="col-sm-12">
-              <div class="row">
-                <div class="col-sm-6">
-                  <label id="email">Email Address</label>
-                  <input
-                    type="text"
-                    placeholder="Email adresa"
-                    class="form-control"
-                    v-model="email"
-                  />
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-6">
-                  <label id="ime">Ime</label>
-                  <input
-                    type="text"
-                    placeholder="Ime"
-                    class="form-control"
-                    v-model="ime"
-                  />
-                </div>
-                <div class="col-sm-6">
-                  <label id="prezime">Prezime</label>
-                  <input
-                    type="text"
-                    placeholder="Prezime"
-                    class="form-control"
-                    v-model="prezime"
-                  />
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-6">
-                  <label id="pass">Lozinka</label>
-                  <input
-                    type="text"
-                    placeholder="Lozinka"
-                    class="form-control"
-                    v-model="password"
-                  />
-                </div>
-                <div class="col-sm-6">
-                  <label id="repeat">Ponovljena lozinka</label>
-                  <input
-                    type="text"
-                    placeholder="Ponovljena lozinka"
-                    class="form-control"
-                    v-model="repeatPassword"
-                  />
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-3">
-                  <label id="year">Godina</label>
-                  <input
-                    type="text"
-                    id="godina"
-                    placeholder="Godina"
-                    class="form-control"
-                    v-model="godina"
-                  />
-                </div>
-                <div class="col-sm-3">
-                  <label id="day">Dan</label>
-                  <input
-                    type="text"
-                    id="mh"
-                    placeholder="Dan"
-                    class="form-control"
-                    v-model="dan"
-                  />
-                </div>
-                <div class="col-sm-3">
-                  <label id="month">Mjesec</label>
-                  <input
-                    type="text"
-                    id="dan"
-                    placeholder="Mjesec"
-                    class="form-control"
-                    v-model="mjesec"
-                  />
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-12">
-                  <p>
-                    Ovim putem potvrđujem ispravnost svih priloženih informacija
-                    i potvrđujem da imam najmanje 18 godina.
-                  </p>
-                  <input type="checkbox" />
-                  <span class="checkmark"></span>
-                  <label>Potvrđujem</label>
-                </div>
-              </div>
-              <div>
-                <button
-                  type="button"
-                  class="btn btn-lg btn-info"
-                  @click="registracija"
-                >
-                  Registracija
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+   
+  <div id="app"> 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">nesto sto treba </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Prijava</router-link>
+          </li>
+          <li class="nav-item active">
+            <router-link to="/singup" class="nav-link"
+              >Registracija</router-link
+            >
+          </li>
+        </ul>
+        <button type="button">stisni</button>
       </div>
-    </div>
+    </nav>
+      <router-view />
+
+       
+    <div> </div>
+     
   </div>
 </template>
+
+<style lang="scss">
+  @import "./assets/style/woody.css";
+  @import "./assets/style/media.css";
+</style>
+
 
 <script>
 import { firebase } from "@/firebase.js";
