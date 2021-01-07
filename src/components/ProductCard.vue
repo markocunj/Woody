19e6ea641775884cd10c8d0fb2f3c49846cf2f82
Pejdo <58657684+Pejdo@users.noreply.github.com>
2021-01-07 17:36:57 +0100

    Update ProductCard.vue

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-md-3 mb-4">
-    <div class="container mx-auto">
+    <div class="container">
       <div class="product">
         <div class="card-body pt-4">
           <h4 class="card-title">{{ info.naslov }}</h4>
@@ -37,80 +37,66 @@
 </template>
 
 <style scoped>
-.container {
-  background-color: #daa520;
-  background-image: url("../assets/background.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
-  opacity: 0.7;
-  width: 100%;
-  height: 500px;
-}
-.col-md-3 {
-  margin-top: 1vw;
-}
-.card-text {
-  height: 12vh;
-}
-.product {
-  background-color: #daa520;
-  margin-left: 30px;
-  margin-right: 30px;
-}
-.col-mid-3 img {
-  width: 70%;
-}
-.align-items-center {
-  -ms-flex-align: center !important;
-  align-items: center !important;
-}
-.justify-content-around {
-  -ms-flex-pack: distribute !important;
-  justify-content: space-around !important;
-}
-.d-flex {
-  display: -ms-flexbox !important;
-  display: flex !important;
-}
-.mb-4,
-.my-4 {
-  margin-bottom: 1.5rem !important;
-}
+  .container {
+    background-color: #daa520;
+    background-image: url("../assets/background.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    opacity: 0.7;
+    width: 100%;
+    height: 500px;
+  }
+  .col-md-3 {
+    margin-top: 1vw;
+  }
+  .card-text {
+    height: 100px;
+  }
+  .product {
+    background: black;
+    transform: translateY(30%);
+  }
+  .align-items-center {
+    -ms-flex-align: center !important;
+    align-items: center !important;
+  }
+  .justify-content-around {
+    -ms-flex-pack: distribute !important;
+    justify-content: space-around !important;
+  }
+  .d-flex {
+    display: -ms-flexbox !important;
+    display: flex !important;
+  }
+  .mb-4,
+  .my-4 {
+    margin-bottom: 1.5rem !important;
+  }
+  *,
+  ::after,
+  ::before {
+    box-sizing: border-box;
+  }
+  .text-center {
+    text-align: center !important;
+  }
+  .pt-4,
+  .py-4 {
+    padding-top: 1.5rem !important;
+  }
+  .red {
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-.row {
-  display: -ms-flexbox;
-  display: flex;
-  -ms-flex-wrap: wrap;
-  flex-wrap: wrap;
-  margin-right: -15px;
-  margin-left: -15px;
-}
-.img-fluid {
-  max-width: 100%;
-  height: auto;
-}
-.img-fluid {
-  max-width: 100%;
-  height: auto;
-}
-img {
-  vertical-align: middle;
-  border-style: none;
-  padding: 30px 30px 0px 30px;
-  box-shadow: 5vw white;
-}
-*,
-::after,
-::before {
-  box-sizing: border-box;
-}
-.text-center {
-  text-align: center !important;
-}
-.pt-4,
-.py-4 {
-  padding-top: 1.5rem !important;
-}
+  @media (max-width: 1270px) {
+    .col-md-3 {
+      max-width: 100%;
+    }
+    .container {
+      min-width: 345px;
+    }
+  }
 </style>
 
 <script>
