4a0950122d2d5edfd2921ae834404d53c14da4bf
markocunj <marko.cunj1@gmail.com>
2021-01-11 15:14:18 +0100

    Update Transitiona izmedu stranica

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@
         </ul>
       </div>
     </nav>
-    <router-view />
+    <transition name="fade" mode="out-in"> <router-view /></transition>
     <WoodyFooter />
   </div>
 </template>
@@ -134,6 +134,17 @@ nav a:hover {
 }
 .navbar-toggler-icon {
   background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3e%3cpath stroke='rgba(255, 255, 255, 0.5)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+}
+//-- Fade izmedu stranica
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
 
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
